92b6783aa9243874db18abac6239fa839a2250e5
Ved Sriraman <sriramanved@gmail.com>
2023-03-11 19:19:18 -0500

    add firebase to this project

---
+++ backend/firebase/firebase.tsx
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { FacebookAuthProvider, getAuth, GoogleAuthProvider, TwitterAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAWCKEkLBpYgN_dIJl6ncEHPngL21j3kz4",
+  authDomain: "okb-hope.firebaseapp.com",
+  projectId: "okb-hope",
+  storageBucket: "okb-hope.appspot.com",
+  messagingSenderId: "321787147976",
+  appId: "1:321787147976:web:190348bb9e96343a7b97fd",
+  measurementId: "G-0EKC171E9X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// Initialize Cloud Firestore through Firebase
+const db = getFirestore(app);
+
+const providers = {
+  google: new GoogleAuthProvider(),
+  facebook: new FacebookAuthProvider(),
+  twitter: new TwitterAuthProvider(),
+};
+
+export { auth, db, analytics };
