89f812508ded354a3e1ca6e669292c66b4329612
mss455 <68719992+swinshu@users.noreply.github.com>
2023-03-17 23:18:21 -0400

    Wrote new cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       // implement node event listeners here
     },
-    baseUrl: "/"
-  },
-});
+    baseUrl: 'http://localhost:3000/'
+  }
+})
