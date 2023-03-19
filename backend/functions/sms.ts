6cabe587db8c6dc28f2865ba22981a43c2f9c91f
Pranav Mishra <pm475@cornell.edu>
2023-03-18 20:28:59 -0400

    init

---
+++ backend/functions/sms.ts
@@ -0,0 +1,11 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    from: '+15017122661',
+    to: '+15558675310'
+  })
+  .then(message => console.log(message.sid));
