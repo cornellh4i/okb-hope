fceff7cb47fe41105e22f46fdfbead295217c692
Pranav Mishra <pm475@cornell.edu>
2023-03-18 21:38:08 -0400

    Added sms messaging w/ Twilio - User argument

--- backend/functions/sms.ts
+++ backend/functions/sms.ts
@@ -1,11 +1,59 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+//Created Twilio Number, authToken, accountSid; Currently on free trial.
+const twilio = require('twilio');
+const accountSid = "AC543c381c5688cda47bf5a20ba6748ace";
+const authToken = "50a7887a0b7a76e46980d9590b9ec9d8";
 const client = require('twilio')(accountSid, authToken);
+const twilioNumber = '+18885215943';
 
-client.messages
-  .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+15017122661',
-    to: '+15558675310'
-  })
-  .then(message => console.log(message.sid));
+
+//TESTING WITH STANDALONE FUNCTION
+// var numbersToMessage = ["+17036384616"]
+
+// numbersToMessage.forEach(function (number) {
+//   var message = client.messages.create({
+//     body: 'OKB Testing Message - Pranav',
+//     from: twilioNumber,
+//     to: number
+//   })
+//     .then(message => console.log(message.status))
+// });
+
+
+//TESTING WITH INTERFACE AND USER
+interface User {
+  phoneNumber: string;
+}
+
+async function sendSMS(user: User, message_body: string) {
+  if (process.env.DATABASE === "staging") {
+    return;
+  }
+  const userPhone = user.phoneNumber;
+  if (userPhone === "Dummy number" || userPhone === undefined)
+    return;
+  try {
+    await client.messages
+      .create({
+        from: twilioNumber,
+        to: userPhone,
+        body: message_body,
+      }).then(message => {
+        message => console.log(message.status);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const example_user: User = { phoneNumber: "+17036384616" };
+sendSMS(example_user, "OKB Test Message 3");
+
+
+//TESTING one message
+// client.messages
+//   .create({
+//     body: 'OKB Testing!',
+//     from: '+18885215943',
+//     to: '+17036384616'
+//   })
+//   .then(message => console.log(message.sid));
