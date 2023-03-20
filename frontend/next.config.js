9a2498571fddb01cb1b857aba5a219b8ac5ecc26
James Kim <biojameskim2002@gmail.com>
2023-03-20 16:03:19 -0400

    Remove unused code and refactor code style

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,36 +5,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// module.exports = nextConfig
-
-// module.exports = nextConfig
-// {
-//   webpack: (
-//     config,
-//     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-//   ) => {
-//     return nextConfig
-//   }
-// }
-
-// module.exports = withFonts({
-//   webpack: (config) => {
-//     // Add the babel-loader rule to transpile your JSX code
-//     config.module.rules.push({
-//       test: /\.(ts|js)x?$/,
-//       exclude: /node_modules/,
-//       use: {
-//         loader: 'babel-loader',
-//         options: {
-//           presets: [
-//             '@babel/preset-env',
-//             '@babel/preset-react',
-//             '@babel/preset-typescript',
-//           ],
-//         },
-//       },
-//     });
-
-//     return config;
-//   },
-// });
+module.exports = nextConfig
