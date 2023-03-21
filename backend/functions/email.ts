402b3f3f207d39b8cdaf65a0e05cefbbdb744192
Andrew Hahn <58017052+hahnandrew@users.noreply.github.com>
2023-03-20 23:51:23 -0400

    Update backend/functions/email.ts
    Co-authored-by: Archit Mehta <4architmehta@gmail.com>

--- backend/functions/email.ts
+++ backend/functions/email.ts
@@ -9,9 +9,5 @@ const msg = {
 }
 sgMail
   .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+  .then(() => console.log('Email sent'))
+  .catch((error) => console.error(error))
