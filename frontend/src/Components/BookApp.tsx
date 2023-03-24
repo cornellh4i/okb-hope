d923f98572e061e9c976cd11d7f60970c5886b2a
Jae Hahn <hahnandy0201@gmail.com>
2023-03-23 20:06:37 -0400

    Added svg's

--- frontend/src/Components/BookApp.tsx
+++ frontend/src/Components/BookApp.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+
 export default function BookApp() {
   return (
     <div>
       <p>
         Book App
       </p>
+      <Image
+        priority
+        src="/Assets/temp_calendar.svg"
+        height={32}
+        width={32}
+        alt="Follow us on Twitter"
+      />
     </div>
   )
 }
