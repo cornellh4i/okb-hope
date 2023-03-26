6baec0abbcd11379893b8a4e3a1fb070cdbec8f3
Ved Sriraman <sriramanved@gmail.com>
2023-03-26 14:30:00 -0400

    added video chat comp

---
+++ frontend/src/components/VideoChat.tsx
@@ -0,0 +1,207 @@
+import React, { useRef, useState } from "react";
+import Peer from "peerjs";
+import { db } from "../../../backend/firebase/firebase";
+
+const VideoChat: React.FC = () => {
+  const webcamButton = useRef<HTMLButtonElement>(null);
+  const callButton = useRef<HTMLButtonElement>(null);
+  const answerButton = useRef<HTMLButtonElement>(null);
+  const hangupButton = useRef<HTMLButtonElement>(null);
+
+  const webcamVideo = useRef<HTMLVideoElement>(null);
+  const remoteVideo = useRef<HTMLVideoElement>(null);
+  const callInput = useRef<HTMLInputElement>(null);
+
+  const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+
+  const servers = {
+    iceServers: [
+      {
+        urls: [
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+        ],
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  };
+
+  const pc = new RTCPeerConnection(servers);
+
+  const setupMediaSources = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    const remoteStream = new MediaStream();
+
+    stream.getTracks().forEach((track) => {
+      pc.addTrack(track, stream);
+    });
+
+    pc.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+      });
+    };
+
+    setLocalStream(stream);
+    setRemoteStream(remoteStream);
+
+    if (webcamVideo.current) {
+      webcamVideo.current.srcObject = stream;
+    }
+
+    if (remoteVideo.current) {
+      remoteVideo.current.srcObject = remoteStream;
+    }
+
+    if (callButton.current && answerButton.current && webcamButton.current) {
+      callButton.current.disabled = false;
+      answerButton.current.disabled = false;
+      webcamButton.current.disabled = true;
+    }
+  };
+
+  const createOffer = async () => {
+    const callDoc = firestore.collection("calls").doc();
+    const offerCandidates = callDoc.collection("offerCandidates");
+    const answerCandidates = callDoc.collection("answerCandidates");
+  
+    if (callInput.current) {
+      callInput.current.value = callDoc.id;
+    }
+  
+    pc.onicecandidate = (event) => {
+      event.candidate && offerCandidates.add(event.candidate.toJSON());
+    };
+  
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+  
+    const offer = {
+      sdp: offerDescription.sdp,
+      type: offerDescription.type,
+    };
+  
+    await callDoc.set({ offer });
+  
+    callDoc.onSnapshot((snapshot) => {
+      const data = snapshot.data();
+      if (!pc.currentRemoteDescription && data?.answer) {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc.setRemoteDescription(answerDescription);
+      }
+    });
+  
+    answerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          const candidate = new RTCIceCandidate(change.doc.data());
+          pc.addIceCandidate(candidate);
+        }
+      });
+    });
+  
+    if (hangupButton.current) {
+      hangupButton.current.disabled = false;
+    }
+  };
+  
+  const answerCall = async () => {
+    const callId = callInput.current?.value;
+    if (!callId) return;
+  
+    const callDoc = firestore.collection("calls").doc(callId);
+    const answerCandidates = callDoc.collection("answerCandidates");
+    const offerCandidates = callDoc.collection("offerCandidates");
+  
+    pc.onicecandidate = (event) => {
+      event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
+  
+    const callData = (await callDoc.get()).data();
+  
+    if (!callData) return;
+  
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+  
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+  
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+  
+    await callDoc.update({ answer });
+  
+    offerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          let data = change.doc.data();
+          pc.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    });
+  };
+  
+  const hangupCall = () => {
+    pc.close();
+  
+    if (webcamButton.current) {
+      webcamButton.current.disabled = false;
+    }
+  
+    if (callButton.current && answerButton.current && hangupButton.current) {
+      callButton.current.disabled = true;
+      answerButton.current.disabled = true;
+      hangupButton.current.disabled = true;
+    }
+  
+    if (callInput.current) {
+      callInput.current.value = "";
+    }
+  
+    setLocalStream(null);
+    setRemoteStream(null);
+    if (webcamVideo.current) {
+      webcamVideo.current.srcObject = null;
+    }
+  
+    if (remoteVideo.current) {
+      remoteVideo.current.srcObject = null;
+    }
+  };
+
+  return (
+    <div>
+      <h1>Video Chat</h1>
+      <video ref={webcamVideo} autoPlay muted playsInline></video>
+      <video ref={remoteVideo} autoPlay playsInline></video>
+      <button ref={webcamButton} onClick={setupMediaSources}>
+        Start webcam
+      </button>
+      <div>
+        <input
+          ref={callInput}
+          type="text"
+          placeholder="Call ID"
+        />
+        <button ref={callButton} onClick={createOffer} disabled>
+          Call
+        </button>
+        <button ref={answerButton} onClick={answerCall} disabled>
+          Answer
+        </button>
+      </div>
+      <button ref={hangupButton} onClick={hangupCall} disabled>
+        Hangup
+      </button>
+    </div>
+  );
+};
+
+export default VideoChat;
---
+++ frontend/src/pages/video-chat.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoChat from '../../components/VideoChat';
+
+const VideoChatPage: React.FC = () => {
+  return (
+    <div>
+      <VideoChat />
+    </div>
+  );
+};
+
+export default VideoChatPage;
