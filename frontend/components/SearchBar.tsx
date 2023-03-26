0d0e4884917e13353b1e639c0154816a228cbe1d
Ved Sriraman <sriramanved@gmail.com>
2023-03-26 11:03:04 -0400

    added search bar component

---
+++ frontend/components/SearchBar.tsx
@@ -0,0 +1,24 @@
+// components/SearchBar.js
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for a psychiatrist or counselor..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
