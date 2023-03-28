b8bec7d1d9dac54ec0d9553d07715756ea930f64
James Kim <biojameskim2002@gmail.com>
2023-03-28 12:36:33 -0400

    Refactor code and extract json file

--- frontend/src/components/QuestionnaireCarousel.tsx
+++ frontend/src/components/QuestionnaireCarousel.tsx
@@ -2,66 +2,41 @@ import { useState } from "react";
 import QuestionCard from "./QuestionCard";
 import NavigationButtons from "./NavigationButtons";
 
-// Some dummy questions
-const questions = [
-  {
-    question: "How are you feeling about your mental health?",
-    options: [
-      'Feeling great!',
-      'Could be better.',
-      "I'm not really sure.",
-      "I'm not doing too well.",
-    ]
-  },
-  {
-    question: "Describe your happiness on a scale of 1-5.",
-    options: [
-      '1',
-      '2',
-      '3'
-    ]
-  },
-  {
-    question: "I want to know how your day is going!",
-    options: [
-      "It couldn't be better!",
-      "It's off to a great start!",
-    ]
-  },
-  {
-    question: "I want to help! How can I best assist you today?",
-    options: [
-      'Tell me more about mental health.'
-    ]
-  }
-]
-
+import questions from "../questions.json"
 
 const QuestionnaireCarousel = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  // This is the amount of questions there are
-  const maxQuestionCount = questions.length;
-  // This indicates the current question we are on.
+  /**
+   * The amount of questions there are
+   */
+  const maxQuestionCount = Object.keys(questions).length;
+  /**
+   * Indicates the current question we are on.
+   */
   const questionCounter = `${currentQuestionIndex + 1} of ${maxQuestionCount}`
 
-  // Updates the question index to go to the previous question.
+  /**
+   * Moves to either the previous question or stays at the first question, 
+   * if we are currently on the first question
+   */
   const goBack = () => {
-    // If we're on the first question, we want to stay at the first question.
-    if (currentQuestionIndex == 0) setCurrentQuestionIndex(0)
-    else setCurrentQuestionIndex(currentQuestionIndex - 1);
+    setCurrentQuestionIndex(currentQuestionIndex == 0 ? 0 :
+      currentQuestionIndex - 1);
   }
 
-  // Updates the question index to go to the next question.
+  /**
+   * Moves to either the next question or stays at the last question, 
+   * if we are currently on the last question
+   */
   const goNext = () => {
-    // If we're on the last question, we want to stay at the last question.
-    if (currentQuestionIndex == maxQuestionCount - 1) setCurrentQuestionIndex(maxQuestionCount - 1)
-    else setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setCurrentQuestionIndex(currentQuestionIndex == maxQuestionCount - 1 ?
+      maxQuestionCount - 1 : currentQuestionIndex + 1);
   }
 
   return (
-    <div>
-      <div className="text-center text-xl mb-4">{questionCounter}</div>
+    <div className="flex-col justify-items-center">
+      <div className="w-96 text-center text-xl mb-4">{questionCounter}</div>
       <QuestionCard
         question={questions[currentQuestionIndex].question}
         options={questions[currentQuestionIndex].options}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from 'next/head';
 
+import QuestionnaireCarousel from '@/components/QuestionnaireCarousel';
+
 export default function Home() {
   return (
     <div className="mx-auto">
       <Head>
         <title>Create Next App</title>
       </Head>
+      <div className='ml-10'>
+        <QuestionnaireCarousel />
+      </div>
+
     </div>
   );
 }
