cff7bdcef3634673225020e822bf80f6f4e31f8e
Ved Sriraman <sriramanved@gmail.com>
2023-03-31 13:19:41 -0400

    converted appointments.json to arr

--- frontend/src/components/AppointmentList.tsx
+++ frontend/src/components/AppointmentList.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import AppointmentCard from './AppointmentCard';
 import results from '../../appointments.json';
 
-
-// { results }
 const AppointmentList = () => {
+  // Convert the results object into an array
+  const appointmentsArray = Object.values(results);
 
-  // const arr = Array.from(Array(results.length).keys)
   return (
-
     <div className="card w-full bg-base-100 shadow-xl">
       <div className="card-body">
         <h1 className="card-title pt-1/15">Upcoming Appointments</h1>
         <div className="grid grid-cols-4 gap-4 items-center pb-1/12 shrink">
-          {[results].map(appointment => (
+          {appointmentsArray.map(appointment => (
             <div key={appointment.id.toString()} className="appointment">
               <AppointmentCard p_name={appointment.name} start={new Date(appointment.start)} end={new Date(appointment.end)} description={appointment.description}></AppointmentCard>
             </div>
@@ -21,18 +19,7 @@ const AppointmentList = () => {
         </div>
       </div>
     </div>
-  )
-
-
-  //   <div className="appointment-list">
-  //     {results.map(appointment => (
-  //       <div key={appointment.id} className="appointment">
-  //         <AppointmentCard p_name={appointment.name[0]} start={new Date(appointment.start)} end={new Date(appointment.end)} description={appointment.description}></AppointmentCard>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
+  );
 };
-
 
 export default AppointmentList;
