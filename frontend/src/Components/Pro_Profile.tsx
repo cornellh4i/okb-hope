d93a16008f5b4259484934d28227e664cd3a0417
Jae Hahn <ah752@cornell.edu>
2023-04-21 06:56:29 -0400

    init'd db data, +'d componenets pro_profile.tsx

--- frontend/src/Components/Pro_Profile.tsx
+++ frontend/src/Components/Pro_Profile.tsx
@@ -3,136 +3,119 @@ import Link_Icon from '../Assets/link_icon.svg'
 import Saved from '../Assets/saved.svg'
 import Message from '../Assets/message.svg'
 import Unsaved from '../Assets/unsaved.svg'
-import pro_profile from "../pro_profile.json"
+import Image from '../Assets/dummy_img.png'
+import { useState } from 'react';
+import Link from 'next/link';
+import TimeTable from './TimeTable'
 
-interface PsychiatristsType {
-  description: string;
-  title: string;
-  name: string;
-  id: string;
-}
-
-
-// const description = collection(db, 'Psychiatrists/`Psychiatrist 1`/description')
-// const name = collection(db, 'Psychiatrists/Psychiatrist 1/name')
-// const title = collection(db, 'Psychiatrists/Psychiatrist 1/title')
-
-// export default function getName() {
-//   const [psychiatristData, setPsychiatristData] = useState<PsychiatristsType[]>([]);
-
-//   async function fetchPsychiatrists() {
-//     const users: PsychiatristsType[] = []
-//     const userSnapshot = await getDocs(collection(db, "Psychiatrists"));
-//     userSnapshot.forEach((doc) => {
-//       users.push({ ...doc.data(), id: doc.id } as PsychiatristsType);
-//     })
-//     // console.log(users[0].name) //jane
-//     // console.log(users[1].name) //janice
-//     // console.log(users[2].name) // ababio
-//     // users is the document array
-//     setPsychiatristData(users);
-//   }
-
-//   useEffect(() => { fetchPsychiatrists() }, []);
-//   // (user => console.log(user.name))
-//   // console.log(psychiatristData[0])
-//   // psychiatristData.forEach(user => console.log(user.title));
-// }
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 interface Props {
-  name: string, 
-  description: string, 
-  title: string, 
-  gender: string, 
-  languages: string[], 
-  photo_id: string
+  name: string,
+  description: string,
+  title: string,
+  gender: string,
+  languages: string[],
+  // photo_id: Element,
+  photo_id: string,
+  link: string,
+  certifications: string,
+  times: any;
 }
-// certifications, link, availabilities, description: string, title: string, gender: string, languages: string[], name: string, photo_id: string
-const Pro_Profile: React.FC<Props> = ({name, description, title, gender, languages, photo_id}) => {
-  
-  let languagesList : JSX.Element[] = [];
+
+const Pro_Profile: React.FC<Props> = ({ name, description, title, gender, languages, photo_id, link, times, certifications }) => {
+
+  let languagesList: JSX.Element[] = [];
 
   languages.forEach((language) => {
     languagesList.push(<div className="badge badge-outline">{language}</div>);
   });
 
-  let daysList : JSX.Element[] = [];
 
-  days.forEach((day) => {
-    daysList.push(<div className="card shadow-xl">
-    <div className="card-body">
-      <h2 className="card-title">{day}</h2>
-    </div>
-  </div>)
-  });
+  // let daysList: JSX.Element[] = [];
+
+  // days.forEach((day) => {
+  //   daysList.push(<div className="card shadow-xl">
+  //     <div className="card-body">
+  //       <h2 className="card-title">{day}</h2>
+  //     </div>
+  //   </div>)
+  // });
+
+  const [saved, setSaved] = useState(false);
+
+  function toggleSaved() {
+    setSaved(!saved);
+  }
 
   return (
     <div>
-      <div className='sm:text-3xl'> {/* this is name*/}
-        {name}
-        {/* should be db title + db name */}
+
+      <div>
+        <Link href="/">
+          <Left_Arrow />
+        </Link>
       </div>
-      <div className=''> {/* img */}
-        {/* <img>{photo_id}</img> */}
-      </div>
 
-      <div className=''> {/* certifications */}
-        <div className="badge badge-outline">
-          {/* {pro_profile[0].certifications} */}
-          {/* json certs */}
+      <div className="flex flex-row">
+        <div>
+          <img src={Image.src} alt="Dummy Image" style={{ width: '800px', height: '350px' }} />
         </div>
-
-        {/* this is save button*/}
-        <button className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3" >
-            <Unsaved />
-            <div>Save Psychiatrist</div>
-        </button>
-
-        {/* this is message button*/}
-        <button className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3">
-          <Message />
-          <div>Message</div>
-        </button>
-        </div>
-
-        {/* gender tag */}
-        <div className="badge badge-outline">
-          {gender}
-        </div>
-
-        {/* language tag */}
-        <div>{languagesList}</div>
-
-        {/* intro paragraph */}
-        <div className=''>
-          {description}
-        </div>
-
-        {/* link, make sure to add svg */}
-        <button className="btn btn-outline">
-          <div>{Link_Icon}</div>
-          <div>www.mentalhealthsite.com</div>
-          {/* <div>{pro_profile[0].link}</div> */}
-        </button>
-    
-        <div className=''> {/* monday?*/}
-          {/*  */}
-          <div className=''> {/* availabilities*/}
-            {/* */}
+        <div>
+          <div className="flex flex-row">
+            <div>
+              <div className="flex flex-col">
+                <p className='sm:text-3xl'>
+                  {title}{name}
+                </p>
+                <p className="italic font-thin">
+                  {certifications}
+                </p>
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-row">
+                <button className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3" onClick={toggleSaved}>
+                  {saved ? <Saved /> : <Unsaved />}
+                  <p>{saved ? 'Saved' : 'Save'}</p>
+                </button>
+                <button className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3">
+                  <Message />
+                  <Link href="/">
+                    Message
+                  </Link>
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='sm:text-3xl'>
-          Availability
-        </div>
-        {/* schedule cards */}
-        <div className='grid grid-flow-col auto-cols-max gap-x-px'>{daysList}</div>
-
-        <div className='sm:text-3xl'>
-          Book Appointment
+          <div className="flex flex-row">
+            <div className="badge badge-outline">
+              {gender}
+            </div>
+            <div>{languagesList}</div>
+          </div>
+          <div className=''>
+            {description}
+          </div>
+          <button className="btn btn-outline bg-[#E5E5E5]">
+            <Link href={link} className="w-18 flex items-center">
+              <Link_Icon /> {link}
+            </Link>
+          </button>
         </div>
       </div>
-  )
+      <div>
+        <p className='sm:text-3xl'>
+          Availability
+        </p>
+        <div className='flex'><TimeTable times={times}></TimeTable></div>
+        <button className="btn col-span-1 text-[16px] flex space-x-3 bg-white text-black border-gray-400">
+          <div>Book Appointment</div>
+        </button>
+      </div>
+    </div >
+  );
+
 }
 
 export default Pro_Profile
--- frontend/src/pages/pro_profile.tsx
+++ frontend/src/pages/pro_profile.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
 import Navbar from '../components/Navbar'
 import Pro_Profile from '../components/Pro_Profile'
-import { useFirestoreDocData } from 'reactfire';
-import { db } from "../../firebase/firebase";
-import { useEffect, useState } from "react";
-import { getDocs, collection, doc, addDoc, setDoc, updateDoc, onSnapshot, getDoc } from "firebase/firestore";
-import 'firebase/firestore';
+import pro_profile from "../pro_profile.json"
+import { usePsychiatrist1 } from "../components/psych1";
 
-// const description = collection(db, 'Psychiatrists/`Psychiatrist 1`/description')
-// const name = collection(db, 'Psychiatrists/Psychiatrist 1/name')
-// const title = collection(db, 'Psychiatrists/Psychiatrist 1/title')
-const psychiatrists = collection(db, 'Psychiatrists')
+const languages: string[] = pro_profile[1].languages
+const gender: string = pro_profile[1].gender
+const link: string = pro_profile[1].link
+const certifications = pro_profile[1].certifications
+const times = pro_profile[1].dates
 
 export default function PsychiatristProfile() {
+  const psychiatrist = usePsychiatrist1();
   return (
     <div>
-      {/* <Navbar /> */}
-      <Pro_Profile name = "Helen Spore" description = "Intro Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." title = "Dr. " gender = "Female" languages = {["English", "Twi"]} photo_id = "null"></Pro_Profile>
-    
-      {/* <Pro_Profile name = {psychiatrists[id].name} description = {psychiatrists[id].description} title = {psychiatrists[id].title} gender = {psychiatrists[id].flags.gender} languages = {psychiatrists[id].flags.languages} photo_id = {psychiatrists[id].photo_id}></Pro_Profile> */}
-    </div>
+      <Navbar />
+      <Pro_Profile name={psychiatrist?.name ?? ""}
+        description={psychiatrist?.description ?? ""}
+        title={psychiatrist?.title ?? ""} gender={gender}
+        languages={languages}
+        photo_id={psychiatrist?.photo_id ?? ""}
+        link={link} times={times}
+        certifications={certifications}></Pro_Profile>
+    </div >
   )
 }
