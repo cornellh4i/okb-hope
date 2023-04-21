9a70a0592ce44aca2614595d45e452164b4c1cb0
mss455 <68719992+swinshu@users.noreply.github.com>
2023-04-21 03:16:25 -0400

    frontend updates/logic fixes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import PsychiatristProfile from "../pages/pro_profile"
 
 export default function Home() {
   return (
     <div className="mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <PsychiatristProfile /> 
       </Head>
     </div>
   );
--- frontend/src/pages/pro_profile.tsx
+++ frontend/src/pages/pro_profile.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Pro_Profile from '../components/Pro_Profile'
+import { useFirestoreDocData } from 'reactfire';
+import { db } from "../../firebase/firebase";
+import { useEffect, useState } from "react";
+import { getDocs, collection, doc, addDoc, setDoc, updateDoc, onSnapshot, getDoc } from "firebase/firestore";
+import 'firebase/firestore';
 
-export default function Bookings() {
+// const description = collection(db, 'Psychiatrists/`Psychiatrist 1`/description')
+// const name = collection(db, 'Psychiatrists/Psychiatrist 1/name')
+// const title = collection(db, 'Psychiatrists/Psychiatrist 1/title')
+const psychiatrists = collection(db, 'Psychiatrists')
+
+export default function PsychiatristProfile() {
   return (
     <div>
-      <Navbar />
-      <Pro_Profile />
+      {/* <Navbar /> */}
+      <Pro_Profile name = "Helen Spore" description = "Intro Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." title = "Dr. " gender = "Female" languages = {["English", "Twi"]} photo_id = "null"></Pro_Profile>
+    
+      {/* <Pro_Profile name = {psychiatrists[id].name} description = {psychiatrists[id].description} title = {psychiatrists[id].title} gender = {psychiatrists[id].flags.gender} languages = {psychiatrists[id].flags.languages} photo_id = {psychiatrists[id].photo_id}></Pro_Profile> */}
     </div>
   )
 }
