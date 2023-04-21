8f194d4fa5aa4b26cf39392cba9d57cb421678b0
Jae Hahn <ah752@cornell.edu>
2023-04-21 17:58:06 -0400

    updated branch so it reflects patient-side-prof

---
+++ frontend/src/pages/pro_profile1.tsx
@@ -0,0 +1,26 @@
+// import Navbar from '../components/Navbar'
+import Pro_Profile from '../components/Pro_Profile'
+import pro_profile from "../pro_profile.json"
+import { usePsychiatrist1 } from "../Components/dummy_psych";
+
+const languages: string[] = pro_profile[1].languages
+const gender: string = pro_profile[1].gender
+const link: string = pro_profile[1].link
+const certifications = pro_profile[1].certifications
+const times = pro_profile[1].dates
+
+export default function PsychiatristProfile() {
+  const psychiatrist = usePsychiatrist1();
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Pro_Profile name={psychiatrist?.name ?? ""}
+        description={psychiatrist?.description ?? ""}
+        title={psychiatrist?.title ?? ""} gender={gender}
+        languages={languages}
+        photo_id={psychiatrist?.photo_id ?? ""}
+        link={link} times={times}
+        certifications={certifications}></Pro_Profile>
+    </div >
+  )
+}
---
+++ frontend/src/pages/pro_profile2.tsx
@@ -0,0 +1,26 @@
+import Navbar from '../components/Navbar'
+import Pro_Profile from '../components/Pro_Profile'
+import pro_profile from "../pro_profile.json"
+import { usePsychiatrist2 } from "../Components/dummy_psych";
+
+const languages: string[] = pro_profile[2].languages
+const gender: string = pro_profile[2].gender
+const link: string = pro_profile[2].link
+const certifications = pro_profile[2].certifications
+const times = pro_profile[2].dates
+
+export default function PsychiatristProfile() {
+  const psychiatrist = usePsychiatrist2();
+  return (
+    <div>
+      <Navbar />
+      <Pro_Profile name={psychiatrist?.name ?? ""}
+        description={psychiatrist?.description ?? ""}
+        title={psychiatrist?.title ?? ""} gender={gender}
+        languages={languages}
+        photo_id={psychiatrist?.photo_id ?? ""}
+        link={link} times={times}
+        certifications={certifications}></Pro_Profile>
+    </div >
+  )
+}
