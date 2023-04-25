e18259f51ea41c79d14f33370e4aac4d85ec8886
Dylan Tom <dt425@cornell.edu>
2023-04-24 23:05:01 -0400

    fixing assets

--- frontend/src/pages/about.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutUs() {
-  return (
-    <div>
-      <p>
-        About Us Page
-      </p>
-    </div>
-  )
-}
--- frontend/src/pages/dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import Dashboard from "@/components/dashboard/Dashboard";
-
-export default function Home() {
-    return (
-        <Dashboard />
-    );
-    }
