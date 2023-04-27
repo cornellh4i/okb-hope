c76b9fdfed9fb132ebdcbc2ab546f47ad29699d1
Jae Hahn <hahnandy0201@gmail.com>
2023-04-26 20:18:30 -0400

    ignore

--- backend/functions/src/auth.ts
+++ backend/functions/src/auth.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+
 const db = admin.firestore();
 
 export const createUserRecord = functions.auth.
--- frontend/src/components/bookings/BookApp.tsx
+++ frontend/src/components/bookings/BookApp.tsx
@@ -1,4 +1,4 @@
-import CalendarIcon from '../../assets/calendar_icon.svg'
+import CalendarIcon from '../../assets/calendar.svg'
 import CameraIcon from '../../assets/camera_icon.svg'
 import ClockIcon from '../../assets/clock_icon.svg'
 import Arrow from '../../assets/arrow.svg'
