edf75a30bc4d2789b0bd8a39e993f9aba58d92f0
Jae Hahn <hahnandy0201@gmail.com>
2023-04-29 19:33:05 -0400

    added additional fields in auth.ts
    Co-authored-by: Michelle Sun <swinshu@users.noreply.github.com>

--- frontend/src/Components/patient_profile/EditPatientProfile.tsx
+++ frontend/src/Components/patient_profile/EditPatientProfile.tsx
@@ -6,9 +6,8 @@ const EditPatientProfile = () => {
     <div className="flex justify-center">
       <div className="card w-2/3">
         <div className="card-body">
-          <text className="card-title text-4xl">Card title!</text>
-          <h1 className="card-title">Card title!</h1>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <text className="card-title text-4xl">Edit Profile</text>
+          <p>hello</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
