90a6d49154ecaecc49e519058d524cb6fc7278c7
Ved Sriraman <sriramanved@gmail.com>
2023-05-07 18:33:24 -0400

    moved loginpopup to separate file

--- frontend/src/components/PsychiatristList.tsx
+++ frontend/src/components/PsychiatristList.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { useAuth } from '../../contexts/AuthContext';
 import { signInWithGoogle } from '../../firebase/firebase';
 import { useRouter } from 'next/router';
+import { LoginPopup } from './LoginPopup';
+
 
 enum Gender {
   Male = 0,
@@ -29,30 +31,6 @@ interface PsychiatristListProps {
   results: Psychiatrist[];
 }
 
-const LoginPopup: React.FC<{ onClose: () => void, signInWithGoogleAndRedirect: (onClose: () => void) => Promise<void> }> = ({ onClose, signInWithGoogleAndRedirect }) => {
-
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="relative bg-white p-8 rounded shadow-md text-center space-y-4">
-        {/* Added "X" button */}
-        <button className="absolute top-2 right-2 text-[#5F5F5F] text-[24px] font-bold" onClick={onClose}>
-          &times;
-        </button>
-        <h2 className="text-xl font-bold">Log in to save, message, and book appointments with psychiatrists</h2>
-        <button className="btn bg-[#E5E5E5] w-full" onClick={() => signInWithGoogleAndRedirect(onClose)}>
-          Login
-        </button>
-        <p>
-          Don't have an account?{' '}
-          <button className="text-[#000000] font-bold" onClick={() => signInWithGoogleAndRedirect(onClose)}>
-            Create one here
-          </button>
-        </p>
-      </div>
-    </div>
-  );
-};
-
 const PsychiatristList: React.FC<PsychiatristListProps> = ({ results }) => {
   const { user } = useAuth();
   const [showPopup, setShowPopup] = useState(false);
@@ -67,9 +45,9 @@ const PsychiatristList: React.FC<PsychiatristListProps> = ({ results }) => {
 
   const signInWithGoogleAndRedirect = async (onClose: () => void) => {
     await signInWithGoogle();
+    router.push('/messages'); // Moved this line before the closing of the popup
     setShowPopup(false);
     onClose();
-    router.push('/messages');
   };
 
   return (
