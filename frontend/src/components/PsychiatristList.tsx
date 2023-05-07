0e4fd18a883686de46446a3f553b6bd795c0504f
Ved Sriraman <sriramanved@gmail.com>
2023-05-07 18:24:19 -0400

    added log in popup functionality

--- frontend/src/components/PsychiatristList.tsx
+++ frontend/src/components/PsychiatristList.tsx
@@ -1,7 +1,10 @@
-// components/PsychiatristList.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import BookMark from '../assets/bookmark.svg'
 import Message from '../assets/message.svg'
+import Link from 'next/link';
+import { useAuth } from '../../contexts/AuthContext';
+import { signInWithGoogle } from '../../firebase/firebase';
+import { useRouter } from 'next/router';
 
 enum Gender {
   Male = 0,
@@ -22,14 +25,56 @@ interface Psychiatrist {
   description: string
 }
 
-
 interface PsychiatristListProps {
   results: Psychiatrist[];
 }
 
+const LoginPopup: React.FC<{ onClose: () => void, signInWithGoogleAndRedirect: (onClose: () => void) => Promise<void> }> = ({ onClose, signInWithGoogleAndRedirect }) => {
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="relative bg-white p-8 rounded shadow-md text-center space-y-4">
+        {/* Added "X" button */}
+        <button className="absolute top-2 right-2 text-[#5F5F5F] text-[24px] font-bold" onClick={onClose}>
+          &times;
+        </button>
+        <h2 className="text-xl font-bold">Log in to save, message, and book appointments with psychiatrists</h2>
+        <button className="btn bg-[#E5E5E5] w-full" onClick={() => signInWithGoogleAndRedirect(onClose)}>
+          Login
+        </button>
+        <p>
+          Don't have an account?{' '}
+          <button className="text-[#000000] font-bold" onClick={() => signInWithGoogleAndRedirect(onClose)}>
+            Create one here
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const PsychiatristList: React.FC<PsychiatristListProps> = ({ results }) => {
+  const { user } = useAuth();
+  const [showPopup, setShowPopup] = useState(false);
+  const router = useRouter();
+
+  const handleSendMessage = (event: React.MouseEvent) => {
+    if (!user) {
+      event.preventDefault();
+      setShowPopup(true);
+    }
+  };
+
+  const signInWithGoogleAndRedirect = async (onClose: () => void) => {
+    await signInWithGoogle();
+    setShowPopup(false);
+    onClose();
+    router.push('/messages');
+  };
+
   return (
     <div className="psychiatrist-list space-y-16">
+      {showPopup && <LoginPopup onClose={() => setShowPopup(false)} signInWithGoogleAndRedirect={signInWithGoogleAndRedirect} />}
       {results.map((psychiatrist) => (
         <div key={psychiatrist.id} className="psychiatrist">
           {/* Display the psychiatrist's information here */}
@@ -43,7 +88,15 @@ const PsychiatristList: React.FC<PsychiatristListProps> = ({ results }) => {
                   <BookMark />
                   <div>Save Psychiatrist</div>
                 </button>
-                <button className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3"><Message /><div>Message</div></button>
+                <Link href="/messages">
+                  <div
+                    className="btn col-span-1 bg-[#E5E5E5] text-[#9A9A9A] text-[16px] flex space-x-3"
+                    onClick={handleSendMessage}
+                  >
+                    <Message />
+                    <div>Message</div>
+                  </div>
+                </Link>
               </div>
               {/* Additional psychiatrist info */}
               <p className="text-[#5F5F5F] text-[16px] font-bold">{psychiatrist.title} at {psychiatrist.location}</p>
@@ -58,3 +111,4 @@ const PsychiatristList: React.FC<PsychiatristListProps> = ({ results }) => {
 };
 
 export default PsychiatristList;
+
