ef0c69b6343a11ba1f752379c2e469cff5d8b2b6
Ved Sriraman <sriramanved@gmail.com>
2023-05-06 20:18:21 -0400

    fixed navbar spacing relative issue

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
-import { logout, signInWithGoogle } from "../../../firebase/firebase";
-import { useAuth } from "../../../contexts/AuthContext";
-import Link from "next/link";
-import Logo from '../../assets/logo.svg'
+import { logout, signInWithGoogle } from '../../../firebase/firebase';
+import { useAuth } from '../../../contexts/AuthContext';
+import Link from 'next/link';
+import Logo from '../../assets/logo.svg';
 
 export default function Navbar() {
   const { user } = useAuth();
@@ -12,38 +12,40 @@ export default function Navbar() {
           {/* logo for website */}
           <Logo />
           <div className="flex-1"></div>
-          <ul className="menu menu-horizontal px-1 gap-5">
+          <ul className="menu menu-horizontal px-1 flex space-x-5">
             <li>
-              {user ? (
+              <Link href="/discover" className="w-18">
+                <div className="text-[18px] font-[500]">Discover Professionals</div>
+              </Link>
+            </li>
+            {user && (
+              <li>
                 <Link href="/messages" className="w-18">
                   <div className="text-[18px] font-[500]">Messages</div>
                 </Link>
-              ) : null}
-            </li>
-            <li>
-              <Link href="/messages" className="w-18">
-                <div className="text-[18px] font-[500]">Messages</div>
-              </Link>
-            </li>
+              </li>
+            )}
             <li>
               <Link href="/about" className="w-18">
                 <div className="text-[18px] font-[500]">About Us</div>
               </Link>
             </li>
             <li>
-              {user ?
+              {user ? (
                 <button
                   className="btn bg-[#E6E6E6] btn-ghost glass w-18 rounded-full bg-base-100"
                   onClick={logout}
                 >
                   Logout
-                </button> :
+                </button>
+              ) : (
                 <button
                   className="btn bg-[#E6E6E6] btn-ghost glass w-18 rounded-full bg-base-100"
                   onClick={signInWithGoogle}
                 >
                   Login
-                </button>}
+                </button>
+              )}
             </li>
           </ul>
         </div>
