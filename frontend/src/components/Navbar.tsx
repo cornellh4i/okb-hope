9ad4de37ea7b91622b25d1aeefecab6ed3db3581
Dylan Tom <dt425@cornell.edu>
2023-08-25 00:12:23 -0400

    fixed navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { logout, signInWithGoogle } from "../../firebase/firebase";
 import { useAuth } from "../../contexts/AuthContext";
 import Link from "next/link";
 import Logo from '@/assets/logo.svg'
+import { sign } from "crypto";
 
 const Navbar = () => {
   const { user } = useAuth();
@@ -40,12 +41,12 @@ const Navbar = () => {
                     <li><a onClick={logout}>Log out</a></li>
                   </ul>
                 </div> :
-                <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-                  <div tabIndex={0} className="btn btn-circle"></div>
-                  <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a onClick={() => signInWithGoogle}>Log in</a></li>
-                  </ul>
-                </div>}
+                <button
+                  className="btn bg-[#195BA5] btn-ghost w-18 rounded-full bg-base-100 text-white"
+                  onClick={() => signInWithGoogle()}
+                >
+                  Log In
+                </button>}
             </li>
           </ul>
         </div>
@@ -57,51 +58,3 @@ const Navbar = () => {
 
 export default Navbar;
 
-
-
-
-    // <header>
-    //   <nav className="">
-    //     <div className="navbar flex bg-[#C1C1C1] items-end">
-    //       <div className="flex-1">
-    //         <img className="btn btn-circle" src={wohohiame_logo}></img>
-    //       </div>
-    //       <div className="flex-1"></div>
-    //       <ul className="menu menu-horizontal px-1 gap-5">
-    //         <li>
-    //           <Link href="/bookings" className="w-18">
-    //             <div className="">Discover Professionals</div>
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/bookings" className="w-18">
-    //             <div className="">Messages</div>
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/bookings" className="w-18">
-    //             <div className="">About Us</div>
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           {user ?
-    //             <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-    //               <img tabIndex={0} className="btn btn-circle"></img>
-    //               <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52">
-    //                 <Link href="edit_profile">
-    //                   <li><a>Edit Profile</a></li>
-    //                 </Link>
-    //                 <li><a onClick={logout}>Log out</a></li>
-    //               </ul>
-    //             </div> :
-    //             <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-    //               <img tabIndex={0} className="btn btn-circle"></img>
-    //               <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52">
-    //                 <li><a onClick={signInWithGoogle}>Log in</a></li>
-    //               </ul>
-    //             </div>}
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </nav >
-    // </header >
--- frontend/src/pages/edit_profile.tsx
+++ frontend/src/pages/edit_profile.tsx
@@ -1,10 +1,8 @@
 import EditPatientProfile from "@/components/patient_profile/EditPatientProfile";
-import Navbar from '../components/navbar/Navbar'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <EditPatientProfile />
     </div>
   );
