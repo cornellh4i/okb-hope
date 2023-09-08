0f65cba709c6ce3a13cbc3e6bad7028831ac8a53
Dylan Tom <dt425@cornell.edu>
2023-09-08 12:14:46 -0400

    Attempt to fix build issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,24 +10,30 @@ const nextConfig = {
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
+    config.module.rules.push({
+      test: /\.json$/,
+      use: {
+        loader: 'json-loader',
+      },
+    });
 
     return config;
   },
 
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://app.infisical.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "https://app.infisical.com" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
