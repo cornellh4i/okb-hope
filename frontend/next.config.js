2e5e711156226d5a05f7148eafea7b1cf3878872
Dylan Tom <dt425@cornell.edu>
2023-09-08 17:04:24 -0400

    slowly fixing lint

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,28 +11,8 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
 
-    config.module.rules.push({
-      test: /\.json$/,
-      loader: 'json-loader',
-    });
-
     return config;
   },
-
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "https://app.infisical.com" },
-  //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-  //       ]
-  //     }
-  //   ]
-  // }
 }
 
 module.exports = nextConfig
