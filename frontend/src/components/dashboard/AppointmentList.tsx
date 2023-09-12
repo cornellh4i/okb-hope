3d8e2ecc093123373d1951a0f9c65c3e642fb0ff
Jae Hahn <hahnandy0201@gmail.com>
2023-09-12 19:14:13 -0400

    fixed json

--- frontend/src/components/dashboard/AppointmentList.tsx
+++ frontend/src/components/dashboard/AppointmentList.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import AppointmentCard from './AppointmentCard';
 import results from '@/temp_data/appointments.json'; // appointment info 
+import NoSavedPsychComponent from './NoSavedPsych';
+import NoUpcomingAppointments from './NoUpcomingAppointments';
+import savedPsych from '@/temp_data/savedpsych.json';
 
 const AppointmentList = () => {
   // Convert the results object into an array
   const appointmentsArray = Object.values(results);
+  const psychNamesArray = savedPsych;
+  // const psychNamesArray = {};
+  // const appointmentsArray = Object.values(emptyresults);
+
+  const appointmentCards = (() => {
+    if (appointmentsArray?.length) {
+      return appointmentsArray.map(appointment => (
+        <div key={appointment.id.toString()} className="appointment">
+          <AppointmentCard
+            p_name={appointment.name}
+            start={new Date(appointment.start)}
+            end={new Date(appointment.end)}
+            description={appointment.description}
+          />
+        </div>
+      ));
+    } else {
+      return NoSavedPsychComponent
+    }
+  })();
 
   return (
     // renders a card containing all of the AppointmentCards 
@@ -13,12 +36,7 @@ const AppointmentList = () => {
         <div className="card-body">
           <h1 className="card-title pt-1/15 text-[32px]">Upcoming Appointments</h1>
           <div className="grid grid-row-4 gap-4 items-center pb-1/12 shrink">
-            {/* map each JSON object to each individual AppointmentCard*/}
-            {appointmentsArray.map(appointment => (
-              <div key={appointment.id.toString()} className="appointment">
-                <AppointmentCard p_name={appointment.name} start={new Date(appointment.start)} end={new Date(appointment.end)} description={appointment.description}></AppointmentCard>
-              </div>
-            ))}
+            {appointmentCards}
           </div>
         </div>
       </div>
