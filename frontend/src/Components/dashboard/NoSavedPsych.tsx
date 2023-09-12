0717078364a95acc9d5c6234e069a1e417266c8e
Jae Hahn <hahnandy0201@gmail.com>
2023-09-12 18:50:28 -0400

    added temp route for rendering

---
+++ frontend/src/Components/dashboard/NoSavedPsych.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoSavedPsych: React.FC = () => {
+  return (
+    <p>
+      You do not have any saved psychologists at the moment. Browse through our list of professional psychologists and save your favorites to see them here.
+    </p>
+  );
+};
+
+export default NoSavedPsych;
---
+++ frontend/src/pages/ah752.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/components/dashboard/Dashboard';
+
+const App = () => {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
