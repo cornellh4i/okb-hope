eb0116c9c47939c4a54e1227306fc5fb0429192f
amywu4 <amywu7162@gmail.com>
2023-09-12 22:26:40 -0400

    onboarding

--- frontend/src/components/aw628.tsx
+++ frontend/src/components/aw628.tsx
@@ -1,7 +1,8 @@
 const HelloWorld = () => {
   return (
-    <div>
+    <div className="border-2 border-blue-700 rounded text-center">
       Hello World
+      <p className="text-red-500">My name is Amy</p>
     </div>
   )
 }
