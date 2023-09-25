255134eecd772eb157a1360c8de8bf4918c238d0
Jacqueline Chin <jc2745@cornell.edu>
2023-09-25 11:49:26 -0400

    updated FilterUserTable.tsx for deleting users

--- frontend/src/components/filter/FilterUserTable.tsx
+++ frontend/src/components/filter/FilterUserTable.tsx
@@ -22,6 +22,34 @@ const FilterUserTable = ({ currentRecords }) => {
     setIsSelected(updatedList);
   };
 
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const popupTextStyle = {
+    color: 'var(--black, #000)',
+    fontFamily: 'Montserrat',
+    fontSize: '24.668px',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: 'normal',
+  };
+
+  const popupTextStyleTwo = {
+    color: 'var(--black, #000)',
+    fontFamily: 'Inter',
+    fontSize: '20.998px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 'normal',
+  };
+
   /**
    * This function removes selected users from the database.
    * @param users An array of users to delete from the firebase collection
@@ -34,7 +62,97 @@ const FilterUserTable = ({ currentRecords }) => {
 
   return (
     <div className="overflow-x-auto">
-      <button className="btn" onClick={() => deleteUsers(isSelected)}>Delete</button>
+      <button
+        className="btn btn-delete"
+        onClick={openPopup}
+        style={{
+          display: 'flex',
+          width: '92px',
+          height: '40px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '10px',
+          background: 'var(--light-grey, #DEDEDE)',
+          color: 'var(--OKB-Blue, #195BA5)',
+          fontFamily: 'Montserrat',
+          fontSize: '20px',
+          fontStyle: 'normal',
+          fontWeight: '600',
+          lineHeight: 'normal',
+        }}
+      >
+        Delete
+      </button>
+
+      {isPopupOpen && (
+        <div
+          className="popup"
+          style={{
+            display: 'flex',
+            width: '531px',
+            height: '250px',
+            padding: '36px',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            gap: '24px',
+            flexShrink: '0',
+            borderRadius: '15.418px',
+            background: 'var(--white, #FFFDFD)',
+            boxShadow: '0px 0px 20px 0px rgba(0, 0, 0, 0.25)',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '999',
+          }}
+        >
+          {/* Content of the popup */}
+          <h2 style={{
+                ...popupTextStyle,
+                marginTop: '36px', /* 36px from the top */
+                marginRight: '36px', /* 36px from the right */
+                marginLeft: '36px', /* 36px from the left */
+              }}>
+            Are you sure you want to delete this user?
+          </h2>
+          <p style={{
+            ...popupTextStyleTwo,
+            marginBottom: '24px', /* 24px from the bottom of <h2> */
+            marginLeft: '36px', /* 36px from the right of the popup */
+            marginRight: '36px', /* 36px from the right of the popup */
+          }}>
+            You will not be able to recover deleted profiles.
+          </p>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button
+              onClick={closePopup}
+              style={{
+                display: 'flex',
+                padding: '12px 16px',
+                alignItems: 'flex-start',
+                gap: '10.453px',
+                borderRadius: '10px',
+                background: 'var(--Light-blue, #519AEB)',
+                color: 'var(--white, #FFFDFD)',
+                fontFamily: 'Montserrat',
+                fontSize: '20px',
+                fontStyle: 'normal',
+                fontWeight: '600',
+                lineHeight: 'normal',
+                width: '68px',
+                height: '24px',
+                position: 'relative',
+                marginRight: '36px', /* 36px from the right of the popup */
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      )}
+
+
       <table className="table w-full">
         {/* head */}
         <thead>
