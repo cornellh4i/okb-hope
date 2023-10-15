182704f918c9c5e505d4591321b70f7b263e5a80
Jacqueline Chin <jc2745@cornell.edu>
2023-10-15 11:37:12 -0400

    finished styling for AppointmentCard

--- frontend/src/components/psychDashboard/AppointmentCard.tsx
+++ frontend/src/components/psychDashboard/AppointmentCard.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+// import tailwindconfig from '../../../tailwind.config.js'
+import okb_colors from '../../colors'
 
 const AppointmentCard = ({p_name, time_start, time_end} : {p_name:string, time_start:string, time_end:string}) => {
   const [isShown, setIsShown] = useState(false);
@@ -7,16 +9,18 @@ const AppointmentCard = ({p_name, time_start, time_end} : {p_name:string, time_s
   };
 
   return (
-    <div className="card flex flex-col w-full bg-base-100 shadow-xl m-6 border-[10px]">
-      <div className="card-body items-center py-2 px-10">
-        <h2 className="card-title">{p_name}</h2>
-        <h2 className="font-[400]">{time_start} {time_end}</h2>
+    <div className="card flex flex-col w-[806px] h-[67px] items-center py-2 px-10 bg-base-100 shadow-[0_0_5px_0_rgba(0,0,0,0.15)] m-6 border-[10px] gap-2.5 self-stretch">
+      <div className="flex justify-between items-center self-stretch">
+        <div className="flex justify-cnter items-center gap-6">
+          <p className="text-black font-montserrat text-xs font-normal">{time_start} - {time_end}</p>
+          <h3 className="text-black font-montserrat text-base font-semibold">{p_name}</h3>
+        </div>
 
         {/* appointment details button */}
-        <div className="card-actions flex">
-          <button>
-
-          </button>
+        <div className={`flex py-1 px-2 justify-center items-center gap-4 rounded-[15px] border border-solid border-[okb_colors.light_blue]`}>
+          <div className="card-actions flex py-0.5 px-2 flex-col items-start gap-2.5 rounded">
+            <p className="font-montserrat text-xs font-normal">Appointment Details</p>
+          </div>
         </div>
       </div>
     </div>
