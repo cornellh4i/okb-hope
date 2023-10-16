0f2bb33cfed82e1709568dc1c5ff67e49280dd73
ai256 <ai256@cornell.edu>
2023-10-16 00:49:37 -0400

    updated frontend

--- frontend/src/components/psychDashboard/PsychiatristDashboard.tsx
+++ frontend/src/components/psychDashboard/PsychiatristDashboard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
-import AppointmentList from '../dashboard/AppointmentList';
+import AppointmentList from './AppointmentList';
 import PsychiatristList from '../psychiatrists/PsychiatristCardsListing';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -14,6 +14,9 @@ const dayOfMonth = currentDate.getDate();
 const dayOfWeek = currentDate.toLocaleString('default', { weekday: 'long' }); // Full day of the week name
 const dateHeaderString = `${month} ${dayOfMonth}, ${year}`;
 const dateString = `${dayOfWeek}, ${month} ${dayOfMonth}`;
+const [defaultValue, setValue] = React.useState<Dayjs | null>(dayjs(currentDate));
+
+
 
 
 const PsychiatristDashboard = () => {
@@ -21,16 +24,26 @@ const PsychiatristDashboard = () => {
         <div className="w-[1440px] h-[1141px] relative bg-neutral-100">
             {/* the side-bar part of dashboard, containing calendar */}
             <div className="w-[264px] h-[274px] bg-white rounded-[10px] shadow" >
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateCalendar defaultValue={dayjs(currentDate)} />
-                </LocalizationProvider>   
+                <div className="w-[216px] h-[226px] relative">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateCalendar value={defaultValue} onChange={(newValue) => setValue(newValue)} />
+                    </LocalizationProvider>   
+                </div>
             </div>
 
             {/* the header part of dashboard, containing week */}
             <div className="w-[1114px] h-[53px] py-3 bg-sky-700 rounded-[10px] justify-center items-center inline-flex">  
-                <div className="w-[271px] h-[29px] left-[45.50px] top-0 absolute justify-start items-center gap-4 inline-flex">
-                    <div className="text-white text-2xl font-semibold font-['Montserrat']">Week of ${dateHeaderString}</div>
-                </div>
+                <div className="relative">
+                    <div className="w-[271px] h-[29px] left-[45.50px] top-0 absolute justify-start items-center gap-4 inline-flex">
+                        <div className="text-white text-2xl font-semibold font-['Montserrat']">Week of {dateHeaderString}</div>
+                    </div>
+                    <div className="w-2.5 h-5 left-0 top-[4px] absolute flex-col justify-start items-start gap-2.5 inline-flex">
+                        <div className="text-center text-white text-xl font-black font-['Font Awesome 6 Free']">angle-left</div>
+                    </div>
+                    <div className="w-2.5 h-5 left-[352px] top-[4px] absolute flex-col justify-start items-start gap-2.5 inline-flex">
+                        <div className="text-center text-white text-xl font-black font-['Font Awesome 6 Free']">angle-right</div>
+                    </div>
+                </div>    
             </div>  
                 
             {/* the main body of dashboard, containing appointments */}
@@ -38,6 +51,7 @@ const PsychiatristDashboard = () => {
                 <div className="left-[467px] top-[193px] absolute flex-col justify-start items-start gap-9 inline-flex">    
                     <div className="relative">
                         <div className="w-[327.76px] left-0 top-0 absolute text-black text-2xl font-semibold font-['Montserrat']">${dateString}</div>
+                        <AppointmentList />
                     </div>
                 </div>
             </div>
