64838561d29f68998732c4158df03d104e820247
Diego Marques <dlm352@cornell.edu>
2023-10-17 16:52:50 -0400

    Took over from Joe, implemented back arrow to /discover, implemented uniform scaling of avalibility times, implemented functionality for site redirect www.mentalhealth.com

--- frontend/src/components/profProfile/ProfProfile.tsx
+++ frontend/src/components/profProfile/ProfProfile.tsx
@@ -8,6 +8,7 @@ import Arrow from '../../assets/return_arrow.svg';
 import Bookmark from '../../assets/bookmark2.svg';
 import Chat from '../../assets/message2.svg';
 import Photo from '../../assets/dummy_photo.jpg';
+import { useRouter } from 'next/router';
 
 
 interface ProfProfileProps {
@@ -21,13 +22,13 @@ const DummyPsychiatrist: IPsychiatrist = {
     last_name: "Shi",
     title: "Psychiatrist at Wohohiame Hospital",
     profile_pic: null,
-    availability: ["9:00-10:00, 13:00-16:30", 
-    "16:00-17:00", 
-    "19:45-21:30, 23:00-23:30", 
-    "8:00-9:00, 15:00-18:00", 
-    "9:00-10:00, 13:00-15:30", 
-    "8:00-9:00, 16:00-18:00, 20:00-21:30", 
-    ""],
+    availability: ["9:00-10:00, 13:00-16:30",
+        "16:00-17:00",
+        "19:45-21:30, 23:00-23:30",
+        "8:00-9:00, 15:00-18:00",
+        "9:00-10:00, 13:00-15:30",
+        "8:00-9:00, 16:00-18:00, 20:00-21:30",
+        ""],
     gender: 1,
     location: "Accra, Ghana",
     language: ["English"],
@@ -40,91 +41,105 @@ const DummyPsychiatrist: IPsychiatrist = {
 }
 
 const ProfProfile = ({ firstName, lastName }: ProfProfileProps) => {
-  const [professional, setProfessional] = useState<IPsychiatrist>(DummyPsychiatrist);
+    const [professional, setProfessional] = useState<IPsychiatrist>(DummyPsychiatrist);
 
-  useEffect(() => {
-    const fetchProfessional = async () => {
-        // We use dummy psychiatrist for now for testing purposes
-        // const data = await fetchProfessionalData(firstName, lastName);
-        // setProfessional(data);
+    useEffect(() => {
+        const fetchProfessional = async () => {
+            // We use dummy psychiatrist for now for testing purposes
+            // const data = await fetchProfessionalData(firstName, lastName);
+            // setProfessional(data);
+        };
+
+        fetchProfessional();
+    }, []);
+
+    const router = useRouter();
+    const handleGoToDashboard = () => {
+        router.push('/discover');
     };
 
-    fetchProfessional();
-  }, []);
+    return (
+        <div className={`w-2/3 h-full flex flex-wrap flex-col justify-center content-center gap-5`}>
+            <div className={`flex flex-row`}>
+                {/* Back arrow to return to go back to Discover Professionals */}
+                <figure className={`cursor-pointer`} onClick={handleGoToDashboard}><Arrow /></figure>
+            </div>
+            <div className={`flex flex-row gap-10`}>
+                <div className={`shrink`}>
+                    <Image src={Photo} alt="Photo" width={1200} height={600} />
+                </div>
+                <div className={`grow flex flex-col gap-4`}>
+                    <div className={`flex flex-row gap-4`}>
+                        <div className={`grow text-3xl text-bold`}>
+                            {professional.first_name + " " + professional.last_name}
+                        </div>
+                        {/* Save button, action is currently undefined */}
+                        <div className={`shrink`}>
+                            <div className={`px-4 py-2 rounded-s-2xl rounded-[12px] bg-okb-blue hover:bg-light-blue transition cursor-pointer text-okb-white flex flex-row gap-2 text-semibold`}>
+                                <figure className="object-cover"><Bookmark /></figure>Save
+                            </div>
+                        </div>
+                        {/* Message button, action is currently undefined */}
+                        <div className={`shrink`}>
+                            <div className={`px-4 py-2 rounded-s-2xl rounded-[12px] bg-okb-blue hover:bg-light-blue transition cursor-pointer text-okb-white flex flex-row gap-2`}>
+                                <figure className="object-cover"><Chat /></figure>Message
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`text-normal text-xl italic text-dark-grey`}>
+                        {professional.title}
+                    </div>
+                    {/* Speciality/language/location tags */}
+                    <div className={`flex flex-row flex-start gap-2`}>
+                        {professional.specialty.map((speciality, index) => (
+                            <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
+                                {speciality}
+                            </div>
+                        ))}
+                        {professional.language.map((langauge, index) => (
+                            <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
+                                {langauge}
+                            </div>
+                        ))}
+                        <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
+                            {professional.location}
+                        </div>
 
-  return (
-    <div className={`w-2/3 h-full flex flex-wrap flex-col justify-center content-center gap-5`}>
-        <div className={`flex flex-row`}>
-            {/* Back arrow to return to go back to Discover Professionals */}
-            <figure className={`cursor-pointer`}><Arrow /></figure>
-        </div>
-        <div className={`flex flex-row gap-10`}>
-            <div className={`shrink`}>
-                <Image src={Photo} alt="Photo" width={1200} height={600}/>
-            </div>
-            <div className={`grow flex flex-col gap-4`}>
-                <div className={`flex flex-row gap-4`}>
-                    <div className={`grow text-3xl text-bold`}>
-                        {professional.first_name + " " + professional.last_name}
                     </div>
-                    {/* Save button, action is currently undefined */}
-                    <div className={`shrink`}>
-                        <div className={`px-4 py-2 rounded-s-2xl rounded-[12px] bg-okb-blue hover:bg-light-blue transition cursor-pointer text-okb-white flex flex-row gap-2 text-semibold`}>
-                            <figure className="object-cover"><Bookmark /></figure>Save
-                        </div>
+                    <div className={`text-normal text-base`}>
+                        {professional.description}
                     </div>
-                    {/* Message button, action is currently undefined */}
-                    <div className={`shrink`}>
-                        <div className={`px-4 py-2 rounded-s-2xl rounded-[12px] bg-okb-blue hover:bg-light-blue transition cursor-pointer text-okb-white flex flex-row gap-2`}>
-                            <figure className="object-cover"><Chat /></figure>Message
+                    <div className={`flex flex-row`}>
+                        {/* Link tag, currently not in the IPsychiatrist so hard coded with default link */}
+                        <div className="px-4 py-2 border-2 rounded-s-2xl rounded-[20px] border-light-blue bg-lightest-blue hover:shadow-xl transition cursor-pointer flex flex-row gap-2">
+                            <a
+                                href="https://www.mentalhealthsite.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center"
+                            >
+                                <figure className="object-cover"><Link /></figure>
+                                <span className="ml-2">www.mentalhealthsite.com</span>
+                            </a>
                         </div>
-                    </div>
-                </div>
-                <div className={`text-normal text-xl italic text-dark-grey`}>
-                    {professional.title}
-                </div>
-                {/* Speciality/language/location tags */}
-                <div className={`flex flex-row flex-start gap-2`}>
-                    {professional.specialty.map((speciality, index) => (
-                        <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
-                            {speciality}
-                        </div>
-                    ))}
-                    {professional.language.map((langauge, index) => (
-                        <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
-                            {langauge}
-                        </div>
-                    ))}
-                    <div className={`px-3 py-2 border-2 rounded-[20px] border-light-blue`}>
-                        {professional.location}
-                    </div>
-                    
-                </div>
-                <div className={`text-normal text-base`}>
-                    {professional.description}
-                </div>
-                <div className={`flex flex-row`}>
-                    {/* Link tag, currently not in the IPsychiatrist so hard coded with default link */}
-                    <div className={`px-4 py-2 border-2 rounded-s-2xl rounded-[20px] border-light-blue bg-lightest-blue hover:shadow-xl transition transition underline cursor-pointer flex flex-row gap-2`}>
-                        <figure className="object-cover"><Link /></figure>www.mentalhealthsite.com
+
                     </div>
                 </div>
             </div>
+            <h2 className={`text-bold text-2xl`}>Availability</h2>
+            <Availability availability={professional?.availability} />
+
+            <div className={`flex flex-row justify-center content-center`}>
+                {/* Book Appointment button, action undefined but should lead to calendly */}
+                <button
+                    className={`bg-okb-blue text-okb-white active:bg-gray-500 font-bold px-12 py-4 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
+                    type="button"
+                >
+                    Book Appointment
+                </button>
+            </div>
         </div>
-        <h2 className={`text-bold text-2xl`}>Availability</h2>
-        <Availability availability={professional?.availability}/>
-        
-        <div className={`flex flex-row justify-center content-center`}>
-            {/* Book Appointment button, action undefined but should lead to calendly */}
-            <button
-                className={`bg-okb-blue text-okb-white active:bg-gray-500 font-bold px-12 py-4 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
-                type="button"
-            >
-                Book Appointment
-            </button>
-        </div>
-    </div>
-  );
+    );
 };
 
 export default ProfProfile;
