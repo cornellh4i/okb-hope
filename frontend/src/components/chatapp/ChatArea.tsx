55f2cf9f1590747ad3ac9559d89f3b2262b961bb
amywu4 <amywu7162@gmail.com>
2023-11-11 21:03:39 -0500

    Finished styling the side bar in Messages page

--- frontend/src/components/chatapp/ChatArea.tsx
+++ frontend/src/components/chatapp/ChatArea.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MessageList from './MessageList';
 import MessageComposer from './MessageComposer';
 import ellipsis from '../../assets/ellipses'
+import okb_colors from '@/colors';
 
 interface NameAreaType {
   name: string;
@@ -11,23 +12,20 @@ interface NameAreaType {
 /** This is the area with Doctor Name, Credentials, and Ellipses button */
 const NameArea = ({ name, credentials }: NameAreaType) => {
   return (
-    <div className='name-area flex justify-between w-full bg-white border-b-solid border-b-2 border-gray-400 rounded-b-md'>
-      <div className='px-5 py-2'>
-        <p className='font-bold text-xl tracking-wide'>{name}</p>
-        <p className='italic text-sm text-gray-400'>{credentials}</p>
+    <div className='name-area flex py-4 px-6 justify-between items-center shrink-0 w-full bg-white border-b-solid border-b-2 border-[#DEDEDE]'>
+      <p className='text-[24px] font-semibold color-black'>{name}</p>
+      <div className="dropdown dropdown-click dropdown-bottom dropdown-end">
+        <button className={`rounded-full color-[${okb_colors.dark_gray}] hover:bg-gray-200`}>
+          {ellipsis}
+        </button>
+        <ul className='menu dropdown-content p-4 shadow bg-base-100 rounded-box w-52'>
+          <li>Mark as Unread</li>
+          <li>View Profile</li>
+          <li>Book Appointment</li>
+          <li>Delete Message Thread</li>
+        </ul>
       </div>
-      <div className = "dropdown dropdown-click dropdown-bottom dropdown-end"> 
-      <button className='rounded-full hover:bg-gray-200'>
-        {ellipsis}
-      </button>
-      <ul className='menu dropdown-content p-4 shadow bg-base-100 rounded-box w-52'>
-      <li>Mark as Unread</li>
-      <li>View Profile</li>
-      <li>Book Appointment</li>
-      <li>Delete Message Thread</li>
-       </ul>
-      </div>
-      
+
     </div>
   );
 }
