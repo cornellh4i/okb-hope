109da9449a7f6f0921c159d00a310bd6d7ce19d1
Jacqueline Chin <jc2745@cornell.edu>
2023-12-02 20:20:27 -0500

    fixed availability card to display time correctly

--- frontend/src/components/profProfile/Availability.tsx
+++ frontend/src/components/profProfile/Availability.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import AvailabilityCard from './AvailabilityCard';
+import { fetchAvailability } from '../../../firebase/fetchData';
+import { IAvailability } from '@/schema';
+
 
 interface AvailabilityProps {
     availability: string[];
@@ -7,20 +10,54 @@ interface AvailabilityProps {
 
 const Availability = ({ availability = [] }: AvailabilityProps) => {
     const days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
-
+    const [availabilities, setAvailabilities] = useState<IAvailability[]>([]);
+    
     // Calculate the maximum number of time slots among all days
     const maxTimeSlots = availability.reduce((max, times) => {
         const timeList = times.split(',').map(time => time.trim());
         return Math.max(max, timeList.length);
     }, 0);
 
+    // Fetch Data of this Availability based on availId 
+    const fetchAvailabilityInfo = async (availId) =>{
+        try{
+            const availData = await fetchAvailability(availId);//dgkPWTpDh87X1q18Pa6E
+            return availData;
+        } catch (error) {
+            console.error("Error fetching data with availId", availId);
+            return undefined;
+        }
+    }
+    useEffect(()=>{
+        // Update state of 'availabilites' with Data of this Availability
+        const fetchDataAvailIds = async () => {
+            const results = await Promise.all(availability.map((availId) => fetchAvailabilityInfo(availId)));
+            const validAvailabilities = results.filter(result => result != null) as IAvailability[];
+            console.log("valid Availabilities" , validAvailabilities);
+            setAvailabilities(validAvailabilities);
+        }
+        fetchDataAvailIds();
+        
+    },[availability]);
+
     return (
         <div className="h-fit">
             <div className={`grid grid-cols-7 gap-8`}>
                 {days.map((day, index) => {
-                    const times = availability[index] || ''; // Ensure times are available or an empty string
-                    const timeList = times.split(',').map(time => time.trim());
-                    const hasTimes = timeList.length > 0;
+                    console.log(index);
+                    // const availField = availability[index] || ''; // Ensure times are available or an empty string
+                    // const timeList = times.split(',').map(time => time.trim());
+                    // const hasTimes = availField.length > 0;
+                    console.log("state of availabilities " , availabilities);
+                    const start = availabilities[index]?.startTime.toDate()
+                    const end = availabilities[index]?.endTime.toDate()
+                    var times = ''
+                    
+                    // check if this availability exists
+                    if (availabilities[index]){
+                        times = `${start.getHours()}:${(start.getMinutes() < 10 ? '0' : '') + start.getMinutes()}-${end.getHours()}:${(end.getMinutes() < 10 ? '0' : '') + end.getMinutes()}` || ''
+                    }
+                    const hasTimes = times.length > 0;
 
                     return (
                         <AvailabilityCard
