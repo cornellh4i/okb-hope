0a5194155aa5619cb73ef57295792ca18a5697e5
leaneying <leaneying23@gmail.com>
2024-02-09 18:20:39 -0500

    Complete routing onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import Button from "@/components/Button";
 
 /** An About page */
 const About = () => {
-  return (
-    <>
-      <Button text="hello" />
-      Hello there
-    </>
-  );
+  return <>Hello there</>;
 };
 
 export default About;
