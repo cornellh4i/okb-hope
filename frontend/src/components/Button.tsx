5898f3fc821fc8536ce6d8aefda8701efc056976
leaneying <leaneying23@gmail.com>
2024-02-09 18:22:01 -0500

    Complete components onboarding

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 
-/**
- * Component for a general button
- * @param text is the text on the button
- */
-
-interface Props {
-  text: string;
-}
-
-const Button = ({ text }: Props) => {
-  return <button>{text}</button>;
+/** A simple Button component */
+const Button = () => {
+  return (
+    <button>Press me</button>
+  );
 };
 
 export default Button;
