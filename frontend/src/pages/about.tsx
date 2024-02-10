12f5d0aef7b9ec753e78e6711b36f61df345d11e
leaneying <leaneying23@gmail.com>
2024-02-09 21:07:09 -0500

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "@/components/Button";
 
+
 const getCatFact = async () => {
   const response = await fetch("https://catfact.ninja/fact");
   console.log(response.json());
@@ -14,6 +15,8 @@ const handleClick = () => {
 const About = () => {
   return (
     <>
+      <div className="bg-black bg-opacity-10 bg-yellow-400 w-3/6 space-x-5 
+      tracking-tighter">Hello</div>
       <button onClick={handleClick}>cat fact</button>
     </>
   );
