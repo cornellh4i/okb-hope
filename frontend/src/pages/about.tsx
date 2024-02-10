68a0075015fb18bd867eca5b76b4a9cbaed9331e
leaneying <leaneying23@gmail.com>
2024-02-09 22:03:02 -0500

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "@/components/Button";
 
 
+
+
 const getCatFact = async () => {
   const response = await fetch("https://catfact.ninja/fact");
   console.log(response.json());
@@ -18,6 +20,12 @@ const About = () => {
       <div className="bg-black bg-opacity-10 bg-yellow-400 w-3/6 space-x-5 
       tracking-tighter">Hello</div>
       <button onClick={handleClick}>cat fact</button>
+
+      <div className="flex flex-col sm:grid sm:grid-cols-2">
+        <div className="bg-red-600 p-4 m-4 rounded-md h-64"></div>
+        <div className="bg-blue-700 p-4 m-4 rounded-md h-64"></div>
+        <div className="bg-green-700 p-4 m-4 rounded-md h-64 sm:col-span-2"></div>
+      </div>
     </>
   );
 };
