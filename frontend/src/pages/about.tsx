4823d437a8ef53cc41a6d84304a931967cc2c48a
sz389 <selenazheng2003@gmail.com>
2024-02-10 19:55:17 -0500

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,6 +5,8 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
+      <div className="bg-opacity-10 bg-yellow-400 w-1/2 tracking-tighter 
+      bg-black">Hello</div>
       <Button text="hello"/>
     </>
   );
