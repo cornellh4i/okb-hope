b543e524b1e65f3673527bbd29db972f8dc0e6a3
leaneying <leaneying23@gmail.com>
2024-03-01 14:55:43 -0500

    finalized front page + selection state with clicking -imported svgs for different states -utilized React useState to determine states
    Co-authored-by: sesebear <sesebear@users.noreply.github.com>

--- frontend/src/components/psychQuestionnaire/SelectionQuestionnaire.tsx
+++ frontend/src/components/psychQuestionnaire/SelectionQuestionnaire.tsx
@@ -4,7 +4,12 @@ import CheckedPsych from '../../assets/checked_psych.svg';
 import UncheckedPatient from '../../assets/unchecked_patient.svg';
 import CheckedPatient from '../../assets/checked_patient.svg';
 
-const SelectionQuestionnaire = ({ setProfile }) => {
+interface SelectionQuestionnaireProps {
+  patient: boolean;
+  psychiatrist: boolean;
+}
+
+const SelectionQuestionnaire = ({ patient, psychiatrist }: SelectionQuestionnaireProps) => {
   const [selectedOption, setSelectedOption] = useState(null);
 
   const handleOptionClick = (option) => {
@@ -12,27 +17,23 @@ const SelectionQuestionnaire = ({ setProfile }) => {
   };
 
   return (
-    <div className={`w-full h-full flex flex-wrap flex-col justify-start gap-6 p-8 pb-4`}>
-      <div className={`text-[32px] font-semibold font-montserrat`}>
+    <div className="w-full h-full flex flex-wrap flex-col justify-start gap-6 p-8 pb-4">
+      <div className="text-[32px] font-semibold font-montserrat">
         Welcome to the Wohoiame Patient Portal!
       </div>
-      <div className={`text-2xl font-semibold font-montserrat`}>
+      <div className="text-2xl font-semibold font-montserrat">
         Are you a patient or a medical professional?
       </div>
-      <div className={`grid grid-cols-2`}>
+      <div className="grid grid-cols-2">
         <div
           className={`flex items-center justify-end ml-2 mr-7 pb-3`}
           onClick={() => handleOptionClick('patient')}
-          onMouseEnter={() => setSelectedOption('patient')}
-          onMouseLeave={() => selectedOption === 'patient' ? null : setSelectedOption(null)}
         >
           {selectedOption === 'patient' ? <CheckedPatient /> : <UncheckedPatient />}
         </div>
         <div
           className={`flex items-center justify-start ml-7 mr-2 pb-3`}
           onClick={() => handleOptionClick('psych')}
-          onMouseEnter={() => setSelectedOption('psych')}
-          onMouseLeave={() => selectedOption === 'psych' ? null : setSelectedOption(null)}
         >
           {selectedOption === 'psych' ? <CheckedPsych /> : <UncheckedPsych />}
         </div>
