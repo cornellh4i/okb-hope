d8aea02b374f01658d4a4d795830b9ae593d1edf
Aadam Faheem <akf35@cornell.edu>
2024-03-01 15:19:00 -0500

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Button from "@/components/Button";
 
-/** An About page */
 const About = () => {
   return (
-    <>
-      <div className="bg-yellow-100 bg-opacity-10 w-1/2 mx-auto text-center tracking-tighter">Hello</div>
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
+      <div className="bg-red-500 rounded-lg p-4 text-white flex justify-center items-center">Red</div>
+      <div className="bg-blue-500 rounded-lg p-4 text-white flex justify-center items-center">Blue</div>
+      <div className="bg-green-500 rounded-lg p-4 text-white flex justify-center items-center sm:col-span-2">Green</div>
+    </div>
   );
 };
 
