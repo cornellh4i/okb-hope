63b2fbd8c0467f5744925745a12dcfd9a914f7fc
Aadam Faheem <akf35@cornell.edu>
2024-03-01 14:57:14 -0500

    Male cats are more likely to be left-pawed, while female cats are more likely to be right-pawed.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
 
-/** A User page */
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(await response.json());
+  };
+
+  const handleClick = () => {
+    getCatFact();
+  };
+
+  return (
+    <>
+      Hello user {userid}!
+      <button onClick={handleClick}>Male cats are more likely to be left-pawed, while female cats are more likely to be right-pawed.</button>
+    </>
+  );
 };
 
 export default User;
