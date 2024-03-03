44fc464ab7c78ec8b2e38b540a9dbfb8a104d3a0
Nicole Fan <nicolefan@dhcp-vl2041-30521.eduroam.cornell.edu>
2024-03-03 17:56:51 -0500

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Button from "@/components/Button";
-
-<div className="bg-yellow">
-opacity-10
-w-1/2
-tracking-tighter
-  Hello</div>
-
+  
 /** An About page */
 const About = () => {
   return (
     <>
       <Button text="hello" />
       Hello there
+      <div className="bg-yellow opacity-10 w-1/2 tracking-tighter"> Hello</div>
+<div className="grid grid-cols-4 sm:grid-cols-2">
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Hello</div>
+  <div>Hello</div>
+</div>
     </>
   );
 };
