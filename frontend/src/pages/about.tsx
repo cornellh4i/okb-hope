916d447865f6170bdba4a10e8a065303c7f4339c
Nicole Fan <nicolefan@dhcp-vl2041-30521.eduroam.cornell.edu>
2024-03-03 17:47:50 -0500

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Button from "@/components/Button";
 
+<div className="bg-yellow">
+opacity-10
+w-1/2
+tracking-tighter
+  Hello</div>
+
 /** An About page */
 const About = () => {
   return (
