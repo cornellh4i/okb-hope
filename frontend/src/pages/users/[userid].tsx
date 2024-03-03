70cec6ba8d90b56808cfde7095475e83f81facff
Nicole Fan <nicolefan@dhcp-vl2041-30521.eduroam.cornell.edu>
2024-03-03 17:40:56 -0500

    The smallest pedigreed cat is a Singapura, which can weigh just 4 lbs (1.8 kg), or about five large cans of cat food. The largest pedigreed cats are Maine Coon cats, which can weigh 25 lbs (11.3 kg), or nearly twice as much as an average cat weighs.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,16 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
+  return <>Hello user {userid}!
+  <button onClick={handleClick}>cat fact</button></>;
 };
 
 export default User;
