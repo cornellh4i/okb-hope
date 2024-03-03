2fe72dc6911cc4b0d94054fff9424c8e51f0e3be
cwon33 <cjw269@cornell.edu>
2024-03-03 16:53:55 -0500

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,7 +6,11 @@ const About = () => {
   return (
     <>
       <div className="bg-yellow-500 opacity-10 w-6/12 tracking-tighter">Hello</div>
-      <Button text="hello" />
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2">
+      <div className="bg-red-500 h-44 flex items-center justify-center rounded-3xl m-6">Red</div>
+      <div className="bg-blue-500 h-44 flex items-center justify-center rounded-3xl m-6">Blue</div>
+      <div className="col-span-1 sm:col-span-2 bg-green-500 h-44 flex items-center justify-center rounded-3xl m-6">Green</div>
+      </div>
     </>
   );
 };
