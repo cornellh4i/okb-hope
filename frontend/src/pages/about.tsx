a865f571910ffb6cf2a417448d1a92ba8ec82315
Nicole Fan <nicolefan@dhcp-vl2041-30521.eduroam.cornell.edu>
2024-03-06 22:37:49 -0500

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,17 +5,11 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
-      <Button text="hello" />
-      Hello there
-      <div className="bg-yellow opacity-10 w-1/2 tracking-tighter"> Hello</div>
-<div className="grid grid-cols-4 sm:grid-cols-2">
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div className="bg-red-600 flex justify-center items-center h-44 rounded-2xl m-5">red</div>
+      <div className="bg-blue-400 flex justify-center items-center h-44 rounded-2xl m-5">blue</div>
+      <div className="col-span-1 sm:col-span-2 bg-green-400 flex justify-center items-center h-44 rounded-2xl m-5">green</div>
+    </div>
     </>
   );
 };
