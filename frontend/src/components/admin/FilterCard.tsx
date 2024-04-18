ec05a28a0628e24caa91d8fe4fb503890faf7010
Nicole Qiu <nfq2@cornell.edu>
2024-04-18 02:57:47 -0400

    added circle warning and check mark

--- frontend/src/components/admin/FilterCard.tsx
+++ frontend/src/components/admin/FilterCard.tsx
@@ -15,29 +15,27 @@ const FilterCard = ({ name, username, created, active, isChecked, onCheckChange 
     border: isChecked ? 'sky-700' : 'gray-300'
   };
 
+  const Icon = name.toLowerCase() === "brianna liu" ? Check : WarningCircle;
+
+
 
   return (
 
     <div className="flex items-center mx-36">
       <div className='flex justify-between items-center w-full border-solid border-2 rounded-lg py-5 px-6' style={cardStyle}>
-        {/* <div className="flex items center">
-            <input type="checkbox" className="checkbox" checked={isChecked} onChange={handleOnChange} />
-            <div className="ml-12 flex items-center">{name}</div>
-          </div>
-          <div className="">{username}</div>
-          <div className="">{created}</div>
-          <div className="pr-40">{active}</div> */}
-        <div className="flex items-center">
+        <div className="flex items-center justify-between w-full">
           <div style={{ width: "40px", marginLeft: "10px" }}>
             <input type="checkbox" className="checkbox" checked={isChecked} onChange={handleOnChange} />
           </div>
-          <div className="flex flex-row" style={{ width: "200px", marginLeft: "20px" }}>
-            <div className="basis-4/5">{name}</div>
-            <div className="basis-1/5"><WarningCircle /></div>
+          <div className="flex items-start w-full">
+            <div className="flex flex-row mr-28" style={{ width: "180px", marginLeft: "20px" }}>
+              <div className="basis-4/5">{name}</div>
+              <div className="basis-1/5"><Icon /></div>
+            </div>
+            <div style={{ width: "180px", marginLeft: "0px" }}>{username}</div>
+            <div style={{ width: "100px", marginLeft: "110px" }}>{created}</div>
+            <div style={{ width: "100px", marginLeft: "240px" }}>{active}</div>
           </div>
-          <div style={{ width: "150px", marginLeft: "180px" }}>{username}</div>
-          <div style={{ width: "150px", marginLeft: "310px" }}>{created}</div>
-          <div style={{ width: "150px", marginLeft: "320px" }}>{active}</div>
         </div>
       </div>
     </div>
