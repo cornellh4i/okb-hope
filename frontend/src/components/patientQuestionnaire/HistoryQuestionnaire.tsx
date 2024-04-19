026ddcb8a1f0ddde0c133a13974b3bd5cfb8d71a
sz389 <sz389@cornell.edu>
2024-04-19 13:06:25 -0400

    concerns fixed

--- frontend/src/components/patientQuestionnaire/HistoryQuestionnaire.tsx
+++ frontend/src/components/patientQuestionnaire/HistoryQuestionnaire.tsx
@@ -5,18 +5,26 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
+import Checkbox from '@mui/material/Checkbox';
+import { FormGroup } from "@mui/material";
 
 interface QuestionnaireProps {
     prevExp: string;
     prevExpTime: string;
-    concerns: string;
+    concerns: string[];
+    check: { [key: string]: boolean };
+    aboutConcerns: string;
+    handleAboutConcerns: (event: ChangeEvent<HTMLTextAreaElement>) => void
+    setConcerns: React.Dispatch<React.SetStateAction<string[]>>;
     handlePrevExp: (event: ChangeEvent<HTMLInputElement>) => void;
     handlePrevExpTime: (event: ChangeEvent<HTMLInputElement>) => void;
-    handleConcerns: (event: ChangeEvent<HTMLInputElement>) => void;
+    setCheck: React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>;
+    handleChecks: (event: ChangeEvent<HTMLInputElement>) => void;
+
 
 }
 // 3rd page of the Questionnaire
-const HistoryQuestionnaire = ({ prevExp, prevExpTime, concerns, handlePrevExp, handlePrevExpTime, handleConcerns }: QuestionnaireProps) => {
+const HistoryQuestionnaire = ({ prevExp, prevExpTime, concerns, check, aboutConcerns, handleAboutConcerns, handlePrevExp, handlePrevExpTime, setConcerns, handleChecks, setCheck }: QuestionnaireProps) => {
     return (
         <div className={`w-full h-full flex flex-wrap flex-col justify-start gap-6 p-8 mb-5`}>
             <div className={`text-[32px] font-semibold font-montserrat`}>
@@ -54,40 +62,96 @@ const HistoryQuestionnaire = ({ prevExp, prevExpTime, concerns, handlePrevExp, h
                     onChange={handlePrevExpTime}
                     className={`flex flex-col gap-2`}
                 >
-                    <FormControlLabel className={`ml-1`} value="Within the last month" control={<Radio />} label={
+                    <FormControlLabel className={`ml-1`} value="within-last-month" control={<Radio />} label={
                         <span style={{ fontWeight: 300, fontSize: 18 }}>
                             Within the last month
                         </span>
                     } />
-                    <FormControlLabel className={`ml-1`} value="Within the last 6 months" control={<Radio />} label={
+                    <FormControlLabel className={`ml-1`} value="within-last-6-months" control={<Radio />} label={
                         <span style={{ fontWeight: 300, fontSize: 18 }}>
                             Within the last 6 months
                         </span>
                     } />
-                    <FormControlLabel className={`ml-1`} value="Within the last year" control={<Radio />} label={
+                    <FormControlLabel className={`ml-1`} value="within-last-year" control={<Radio />} label={
                         <span style={{ fontWeight: 300, fontSize: 18 }}>
                             Within the last year
                         </span>
                     } />
-                    <FormControlLabel className={`ml-1`} value="Over a year ago" control={<Radio />} label={
+                    <FormControlLabel className={`ml-1`} value="over-a-year-ago" control={<Radio />} label={
                         <span style={{ fontWeight: 300, fontSize: 18 }}>
                             Over a year ago
                         </span>
                     } />
-                    <FormControlLabel className={`ml-1`} value="I have never spoken with a counselor/therapist before" control={<Radio />} label={
+                    <FormControlLabel className={`ml-1`} value="never-spoken" control={<Radio />} label={
                         <span style={{ fontWeight: 300, fontSize: 18 }}>
                             I have never spoken with a counselor/therapist before.
                         </span>
                     } />
                 </RadioGroup>
             </FormControl>
-
-            <div>
-                <span className={`text-lg font-semibold font-montserrat`}>Are there any specific concerns you would like to discuss with your conselor?</span>
-                <input type="text" value={concerns} onChange={handleConcerns} placeholder="Type here" className={`input input-bordered w-full mr-3 border-2 rounded-2xl`} style={{ borderColor: okb_colors.light_blue }} />
-            </div>
-
-
+            <FormGroup>
+                <div>
+                    <span className={`text-lg font-semibold font-montserrat`}>Are there any specific concerns you would like to discuss with your conselor?</span>
+                    <span className={`text-lg text-red-600`}>*</span>
+                </div>
+                <FormControlLabel control={<Checkbox defaultChecked={check.MyRelationships} checked={check['My Relationships']} value={`My Relationships`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        My Relationships
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.Addiction} checked={check['Addiction']} value={`Addiction`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Addiction
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.SuicidalThoughts} checked={check['Suicidal Thoughts']} value={`Suicidal Thoughts`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Suicidal Thoughts
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.FamilyDistress} checked={check['Family Distress']} value={`Family Distress`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Family Distress
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.SubstanceAbuse} checked={check['Substance Abuse']} value={`Substance Abuse`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Substance Abuse
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.AcademicDistress} checked={check['Academic Distress']} value={`Academic Distress`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Academic Distress
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.SocialAnxiety} checked={check['Social Anxiety']} value={`Social Anxiety`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Social Anxiety
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.Depression} checked={check['Depression']} value={`Depression`} onChange={handleChecks} />} className={` ml-1 `} label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Depression
+                    </span>
+                } />
+                <FormControlLabel control={<Checkbox defaultChecked={check.Other} checked={check['Other']} value="Other" onChange={handleChecks} />} className="ml-1" label={
+                    <span style={{ fontWeight: 300, fontSize: 18 }}>
+                        Other
+                    </span>
+                } />
+                {check.Other && (
+                    <textarea
+                        value={aboutConcerns}
+                        onChange={handleAboutConcerns}
+                        placeholder="Type here"
+                        className={`input input-bordered w-full mr-3 border-2 rounded-2xl`}
+                        style={{
+                            borderColor: okb_colors.light_blue,
+                            height: 45,
+                        }}
+                        />
+                      )}
+            </FormGroup>
         </div>
     )
 };
