006b773e2c07b66e3ef7b64ff04cdb9d664cb028
sz389 <sz389@cornell.edu>
2024-04-19 13:20:39 -0400

    changes to firebase

--- frontend/firebase/firebase.tsx
+++ frontend/firebase/firebase.tsx
@@ -66,6 +66,7 @@ const signUpWithGoogle = async (
   lastTherapyTimeframe: string,
   ageRange: string,
   prefLanguages: string[],
+  genderPref: GenderOrUndefined,
   savedPsychiatrists: string[],
 ) => {
   return new Promise<void>(async (resolve, reject) => {
@@ -115,7 +116,7 @@ const signUpWithGoogle = async (
             lastTherapyTimeframe: lastTherapyTimeframe,
             ageRange: ageRange,
             prefLanguages: prefLanguages,
-            gender: gender,
+            genderPref: genderPref,
             savedPsychiatrists: savedPsychiatrists
           });
           console.log("Added patient")
