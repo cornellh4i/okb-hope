2742b6bbf71ce4a2686250aa52b2544ae5a7c9af
Sophie Strausberg <sophie@dhcp-vl2041-43828.eduroam.cornell.edu>
2024-04-26 13:47:30 -0400

    fix messaging bugs

--- frontend/src/components/chatapp/SearchBar.tsx
+++ frontend/src/components/chatapp/SearchBar.tsx
@@ -15,14 +15,14 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      onSearch(searchInput.trim()); // Pass search input value to parent component
-      console.log(searchInput)
+        onSearch(searchInput.trim()); // Pass search input value to parent component
+        console.log(searchInput)
     }
   };
 
   return (
     <div className={`search-bar`}>
-      <div className={`flex py-2 md:px-4 px-4 items-center md:gap-4 gap-2 shrink-0 rounded-lg justify-between search-form border-solid border-[1px] border-[${okb_colors.dark_gray}] md:mx-4 my-5 text-lg font-semibold`}>
+      <div className={`flex py-2 md:px-4 px-2 items-center md:gap-4 gap-2 shrink-0 rounded-lg justify-between search-form border-solid border-[1px] border-[${okb_colors.dark_gray}] md:mx-4 my-5 text-lg font-semibold`}>
         <button onClick={() => onSearch(searchInput)}> {search_icon}</button> {/* This line seems redundant. If you need it for something specific, you can leave it. */}
         <input
           className={`text-[${okb_colors.med_gray}] page-background italic text-xs font-normal outline-none w-full`}
