88401154f8ab3b4f38943939414d60cbc46ecd88
alishav14 <alishavarma1234@gmail.com>
2024-06-02 12:59:56 -0400

    Implemented admin features

--- frontend/src/components/adminReport/ReportPopup.tsx
+++ frontend/src/components/adminReport/ReportPopup.tsx
@@ -1,4 +1,4 @@
-const ReportPopup = ({ report }) => {
+const ReportPopup = ({ report, showPatientsReports }) => {
   const getFormattedDate = (date) => {
     if (!date) return 'Unknown date';
     try {
@@ -23,7 +23,7 @@ const ReportPopup = ({ report }) => {
     <div style={cardStyle} className="card bg-base-100  mb-4">
       <div>
         <h2 style={{ marginBottom: '10px' }} className="font-bold font-montserrat">Submitted By: <span className="font-normal">{report.reporter_name}</span> </h2>
-        <h2 style={{ marginBottom: '10px' }} className="font-bold font-montserrat">Psychiatrist Reported: <span className="font-normal">Dr. {report.psych_name}</span> </h2>
+        <h2 style={{ marginBottom: '10px' }} className="font-bold font-montserrat">Person Reported: <span className="font-normal">{showPatientsReports ? report.patient_name : `Dr. ${report.psych_name}`}</span> </h2>
         <h2 style={{ marginBottom: '10px' }} className="font-bold font-montserrat">Submitted On: <span className="font-normal">{formattedDate}</span></h2>
         <p style={{ fontSize: '14px', border: '1px solid #c7c5c5', color: '#000000', padding: '8px 12px' }} className="font-normal font-montserrat"> {report.description}</p>
       </div>
