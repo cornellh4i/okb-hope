2fbe76822d7f3ca548da687edd95ce99676e6854
Victoria Yu <vwy5@cornell.edu>
2024-11-19 16:28:07 -0500

    Fixed syntax issues of apostrophes, etc

--- frontend/src/components/profProfile/SimilarPsychCard.tsx
+++ frontend/src/components/profProfile/SimilarPsychCard.tsx
@@ -1,14 +1,15 @@
-import { useState, useEffect } from ‘react’;
-import { IPsychiatrist } from ‘@/schema’;
-import { useRouter } from ‘next/router’;
-import { fetchUnreportedProfessionals } from ‘../../../firebase/fetchData’;
-import PsychiatristList from ‘@/components/psychiatrists/PsychiatristList’;
-import { useAuth } from ‘../../../contexts/AuthContext’;
+import { useState, useEffect } from 'react';
+import { IPsychiatrist } from '@/schema';
+import { useRouter } from 'next/router';
+import { fetchUnreportedProfessionals } from '../../../firebase/fetchData';
+import PsychiatristList from '@/components/psychiatrists/PsychiatristList';
+import { useAuth } from '../../../contexts/AuthContext';
 
 const SimilarPsychiatristsDisplay: React.FC = () => {
   const router = useRouter();
   const { user } = useAuth();
   const [psychiatrists, setPsychiatrists] = useState<IPsychiatrist[]>([]);
+
   useEffect(() => {
     async function fetchData() {
       try {
@@ -22,18 +23,20 @@ const SimilarPsychiatristsDisplay: React.FC = () => {
     }
     fetchData();
   }, [user]);
+
   return (
-    <div className=“flex flex-col px-23 pt-9 pb-14”>
-      <div className=“flex justify-center pb-8">
+    <div className="flex flex-col px-23 pt-9 pb-14">
+      <div className="flex justify-center pb-8">
         {psychiatrists.length > 0 ? (
-          <PsychiatristList results={psychiatrists} buttonType={‘discover’} />
+          <PsychiatristList results={psychiatrists} buttonType={'discover'} />
         ) : (
-          <div className=“text-center my-10”>
-            <p className=“mb-4">No Psychiatrists found.</p>
+          <div className="text-center my-10">
+            <p className="mb-4">No Psychiatrists found.</p>
           </div>
         )}
       </div>
     </div>
   );
 };
+
 export default SimilarPsychiatristsDisplay;
